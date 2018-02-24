31dae157984fbf523ae9a9fde3b486d041c15c36
Carl Hultberg <carl.johan.hultberg@gmail.com>
2018-02-24 15:19:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,16 @@ class RoomView():
         self.frame = Frame(master)
 
         self.lb_room = Listbox(self.frame, selectmode=SINGLE)
-
+        self.label_Participants = Label(self.frame, text="Participants")
+        self.label_Participants.grid(row=1, column=2)
         self.lb_room.config(exportselection=False) #no deselect when clicking outside box
+        self.lb_room.config(width=50, height=20)
         self.lb_room.bind('<<ListBoxSelect>>', self.selected_room)
-        self.lb_room.grid(row=0, column=0)
+        self.lb_room.grid(row=20, column=2)
+        self.btn_booking = Button(self.frame, text="Go back to start", command=self.Back_menu)
+        self.btn_booking.grid(row=0, column=2)
 
-        cur.execute("SELECT * FROM room")
+        cur.execute("SELECT Resources.Room_ID, Stuff, Cost FROM Resources INNER JOIN Facilities ON Resources.Room_ID = Facilities.Room_ID WHERE IsBooked = 0")
         rooms = cur.fetchall()
         column = 0
         for room in rooms:
@@ -37,6 +41,11 @@ class RoomView():
     def hide(self):
         self.frame.grid_forget()
 
+    def Back_menu(self):
+        t = LoginWindow(self.master)
+        self.hide()
+        t.show()
+
     def selected_room(self, evt):
         w = evt.widget
         index = int(w.curseselection()[0])
@@ -44,7 +53,41 @@ class RoomView():
 
         self.hide()
 
+class RemoveView():
+    def __init__(self, master):
+        self.master = master
+        self.frame = Frame(master)
 
+        self.lb_room = Listbox(self.frame, selectmode=SINGLE)
+        self.lb_room.config(exportselection=FALSE)
+        self.lb_room.bind('<<ListBoxSelect>>', self.select_booked_room)
+        self.lb_room.grid(row=0,column=0)
+        self.btn_booking = Button(self.frame, text="Go back to start", command=self.Back_menu)
+        self.btn_booking.grid(row=1, column=6)
+
+        cur.execute("SELECT * FROM room")
+        rooms = cur.fetchall()
+        for room in rooms:
+            self.lb_room.insert(END, room)
+
+
+    def show(self):
+        self.frame.grid(row=0, column=0)
+
+    def hide(self):
+        self.frame.grid_forget()
+
+    def Back_menu(self):
+        t = LoginWindow(self.master)
+        self.hide()
+        t.show()
+
+    def select_booked_room(self, evt):
+        w = evt.widget
+        index = int(w.curseselection()[0])
+        room = w.get(index)
+
+        self.hide()
 
 
 class LoginWindow():
@@ -58,8 +101,8 @@ class LoginWindow():
 
         self.btn_booking = Button(self.frame, text="Make Booking", command=self.make_booking)
         self.btn_booking.grid(row=2, column=0)
-        #self.btn_remove = Button(self.frame, text="Remove Booking", command=self.remove_booking)
-        #self.btn_remove.grid(row=1, column=1)
+        self.btn_remove = Button(self.frame, text="Remove Booking", command=self.remove_booking)
+        self.btn_remove.grid(row=2, column=1)
 
     def show(self):
         self.frame.grid(row=0, column=0)
@@ -72,6 +115,10 @@ class LoginWindow():
         self.hide()
         t.show()
 
+    def remove_booking(self):
+        t = RemoveView(self.master)
+        self.hide()
+        t.show()
 
 
 root = Tk()
